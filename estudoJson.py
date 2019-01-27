1a951a826be3cb37672513ca3fef7a6d9139a23d
FabioJRMoreira <tulkas.fabio@hotmail.com>
2019-01-27 18:27:04 -0200

    Add files via upload

---
+++ estudoJson.py
@@ -0,0 +1,31 @@
+import json
+import urllib.request
+
+
+def carrega_json():
+    arquivo = urllib.request.urlopen('https://api-sandbox.contabilizei.com/ds/customers')
+
+    js = arquivo.read()
+
+    info = json.loads(js.decode("utf-8"))
+
+    companias = []
+
+    for intens in info:
+        cp=[]
+        cp.append(intens['company_Id'])
+        for stat in intens['company_state']:
+            cp.append(stat)
+        for produtos in intens['Products_list']:
+            for produto in produtos['product']:
+                cp.append(produto)
+            cp.append(produtos['product_price'])
+        companias.append(cp)
+    return companias
+
+def busca_empresa(estado ,produto):
+    for comp in carrega_json():
+        if(produto in comp and estado in  comp):
+            print(comp)
+
+busca_empresa('SP','mousse')
